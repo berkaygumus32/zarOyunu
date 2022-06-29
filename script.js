d7e50f9ee4a0cbf2da3e7ce5f44eacac47583f45
Berkay <berkaygumus32@gmail.com>
2022-06-29 16:30:10 -0600

    game is ready

---
+++ script.js
@@ -0,0 +1,29 @@
+// first image -----------------------------------------------------
+
+const randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+const randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1 to dice2
+
+const randomImageSource = "images/" + randomDiceImage; //images/dice1.png images/dice6.png
+
+const image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+// second image -----------------------------------------------------
+
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;  //random # 1-6
+
+// const randomDiceImage2 = "dice" + randomNumber2 + ".png"
+
+const randomImageSource2 = "images/dice" + randomNumber2 + '.png'; //images/dice1.png to dice6.png
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!"
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+} else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
